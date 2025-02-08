b6bec2cb59d7366e6fab8b9440b87e854deb9d9f
Thanh Le <lnthanh@fit.hcmus.edu.vn>
2025-02-08 18:20:45 +0700

    Add files via upload

---
+++ ccgcn/compgcn_conv.py
@@ -0,0 +1,7 @@
+from utils import *
+from message_passing import MessagePassing
+from torch_scatter import scatter_add
+
+
+class CompGCNConv(MessagePassing):
+	The code will be made available after the first round of review.
